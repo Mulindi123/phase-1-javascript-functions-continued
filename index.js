1044615cb447f1756e0d24f3b1102fb25c583748
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-07-12 02:02:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+const saturdayFun = function(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+    }
+
+const mondayWork = function(job = 'go to the office') {
+        return `This Monday, I will ${job}.`;
+    }
+    function wrapAdjective(flair ='*'){
+        return function(message ='special'){
+            return `You are ${flair}${message}${flair}!`;
+        }
+    }
+    const encouragingPromptFunction = wrapAdjective("!!!");
+     console.log(encouragingPromptFunction("a dedicated programmer"));
